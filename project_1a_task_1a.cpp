b1bd553ad093ac7b9722a01cc119529801d2259e
wslaybau <wslaybau@ucsc.edu>
2026-02-22 00:50:27 -0800

    Attempting to practice with lambdas in order to reduce the amount of code and simplify the scatter process.

--- project_1a_task_1a.cpp
+++ project_1a_task_1a.cpp
@@ -4,15 +4,50 @@
 #include <utility>
 #include <fstream>
 
-std::pair<long long, long long> Fib(int k);
+auto Fib = [](auto&& self, int k) -> std::pair<long long, long long>{
+    if (k == 0) {
+        return {0, 0};
+    }
+
+    if (k == 1) {
+        return {1, 0};
+    }
+
+    auto left = self(self, k-1);
+    auto right = self(self, k-2);
+    long long kthfib = left.first + right.first;
+    long long additions = left.second + right.second + 1;
+    return {kthfib,additions};
+};
+
+template <typename T>
+int Scatter(int k, T&& Func)
+{
+    std::ofstream file("Fib_results.csv");
+    if (!file){
+        std::cout << "Error opening file." << std::endl;
+        return 1;
+    }
+
+    file << "k, F(k), A(k)\n";
+    for (size_t i = 0; i <= k; i++){
+        std::pair<long long, long long> result = Func(Func, i);
+        file << i << "," << result.first << "," << result.second << "\n";
+    }
+
+    file.close();
+    std::cout << "Results written to Fib_results.csv" << std::endl;
+    return 0;
+}
 
 int main(){
+
     int choice = 0;
     int k;
 
     std::cout << "Fib(k). Enter 0 for user mode, 1 for scatterplot mode: ";
     std::cin >> choice;
-    
+
     if (!choice){
         std::cout << "Calculating the fibonacci number k. Please choose k, 0 <= k <= 92 : ";
         std::cin >> k;
@@ -22,7 +57,7 @@ int main(){
             return 1;
         }
 
-        std::pair<long long, long long> result = Fib(k);
+        std::pair<long long, long long> result = Fib(Fib, k);
         std::cout << "Results written to console: k, Fib(k), A(k)." << std::endl << k << "," << result.first << "," << result.second << "\n";
         return 0;
     } else {
@@ -33,37 +68,9 @@ int main(){
             std::cout << "0 <= k <= 92 only. Terminating program." << std::endl;
             return 1;
         }
-    
-        std::ofstream file("Fib_results.csv");
-        if (!file){
-            std::cout << "Error opening file." << std::endl;
-            return 1;
-        }
 
-        file << "k, F(k), A(k)\n";
-        for (size_t i = 0; i <= k; i++){
-            std::pair<long long, long long> result = Fib(i);
-            file << i << "," << result.first << "," << result.second << "\n";
-        }
-    
-        file.close();
-        std::cout << "Results written to Fib_results.csv" << std::endl;
-        return 0;
+        Scatter(k, Fib);
+
     }
 }
 
-std::pair<long long, long long> Fib(int k){ 
-    if (k == 0) {
-        return {0, 0};
-    }
-
-    if (k == 1) {
-        return {1, 0};
-    }
-
-    auto left = Fib(k-1);
-    auto right = Fib(k-2);
-    long long kthfib = left.first + right.first;
-    long long additions = left.second + right.second + 1;
-    return {kthfib,additions};
-}
